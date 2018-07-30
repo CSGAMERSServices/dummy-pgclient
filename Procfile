c3f0fa0cc484f13d879a4f4807e9101761dda853
Daniel Feinberg <daniel.feinberg@cloudwareconnections.com>
2018-07-30 18:08:55 -0400

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+connector: node index.js
---
+++ index.js
@@ -0,0 +1,44 @@
+const pg = require('pg');
+const env = require('env-var');
+const winston = require('winston');
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+
+const logger = winston.createLogger({
+    level: 'silly',
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.splat(),
+        winston.format.simple()
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+function initializePGConnection() {
+    var conString = env.get("DATABASE_URL").required().asString();
+    var client = new pg.Client({
+        connectionString: conString,
+        ssl: true
+        })
+
+    client.connect((err) => {
+        if (err) {
+            logger.error(`Failed to connect to database ${conString} with error ${err}`);
+            process.exit(1);
+        } else {
+            logger.info(`Connection to database ${conString} has been established successfully`);
+            process.exit(0);
+        }
+    })
+}
+
+initializePGConnection();
